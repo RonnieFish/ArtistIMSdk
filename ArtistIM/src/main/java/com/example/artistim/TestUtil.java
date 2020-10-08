df338a3f0a98d975f6a0db7061de57a5df9532df
李政桐 <1737576261@qq.com>
2020-10-08 22:05:28 +0800

    testutil

--- ArtistIM/src/main/java/com/example/artistim/TestUtil.java
+++ ArtistIM/src/main/java/com/example/artistim/TestUtil.java
@@ -1,4 +1,27 @@
 package com.example.artistim;
 
+import android.util.Log;
+
 public class TestUtil {
+    private static volatile TestUtil instance = null;
+
+    public TestUtil getInstance(){
+        if(instance==null){
+            synchronized (TestUtil.class){
+                if(instance == null){
+                    instance = new TestUtil();
+                }
+
+            }
+        }
+        return instance;
+    }
+
+    private TestUtil(){
+
+    }
+
+    public void haha(){
+        Log.d("jjyyjjyys", "haha: ");
+    }
 }
