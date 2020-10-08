b5940a13a9c0cfa0577cf1536cc91aff9eab070e
李政桐 <1737576261@qq.com>
2020-10-08 22:04:56 +0800

    Initial commit

---
+++ ArtistIM/src/main/java/com/example/artistim/TestUtil.java
@@ -0,0 +1,4 @@
+package com.example.artistim;
+
+public class TestUtil {
+}
---
+++ app/src/main/java/com/example/artistimsdk/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.artistimsdk;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+
+import com.example.artistim.ChatActivity;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ChatActivity.class));
+            }
+        });
+    }
+}
